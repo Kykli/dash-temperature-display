a424e1fe0c9f015e1b152b95a91353f6fa78b17d
Kykli <luukkanen.eetu@gmail.com>
2018-08-26 18:31:42 +0300

    DatePickerRange works now with the graph

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import dash
 from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
+import plotly.graph_objs as go
 import datetime as dt
 import pandas_datareader.data as web
 import pandas as pd
@@ -54,24 +55,13 @@ app.layout = html.Div(style={"backgroundColor": colors["background"]}, children=
     dcc.DatePickerRange(
         id="date-picker-range",
         start_date=dt.datetime(2018, 5, 22),
-        end_date=dt.datetime(2018, 8, 13),
+        end_date=dt.datetime.now(),
         min_date_allowed=dt.datetime(2018, 5, 22),
-        max_date_allowed=dt.datetime(2018, 8, 13),
+        max_date_allowed=dt.datetime.now(),
         end_date_placeholder_text="Select a date"
     ),
 
     dcc.Graph(id="in-temp-graph")
-#        figure={
-#            "data": [
-#                {"x": df.date, "y": df.temperature, "type": "line", "name": iTemperature}, 
-#            ],
-#            "layout": {
-#                "title": iTemperature,
-#                "plot_bgcolor": colors["graphBackground"],
-#                "paper_bgcolor": colors["graphBackground"]
-#            }
-#        }
-#    )
 
 ])
 
@@ -82,9 +72,12 @@ app.layout = html.Div(style={"backgroundColor": colors["background"]}, children=
 )
 def update_graph(start_date, end_date):
     
+    start_date = pd.to_datetime(start_date)
+    end_date = pd.to_datetime(end_date)
+ 
     filtered_df = df2[df2.date.between(
-        dt.datetime.strptime(start_date, "%Y-%m-%d"),
-        dt.datetime.strptime(end_date, "%Y-%m-%d")
+        dt.datetime.strftime(start_date, "%Y-%m-%d"),
+        dt.datetime.strftime(end_date, "%Y-%m-%d")
     )]
 
     trace1 = go.Scatter(
@@ -95,14 +88,13 @@ def update_graph(start_date, end_date):
     )
 
     return {
-        "data": trace1,
+        "data": [trace1],
         "layout": go.Layout(
             title = iTemperature,
             plot_bgcolor = colors["graphBackground"],
             paper_bgcolor = colors["graphBackground"]
         )
     }
-
 
 
 if __name__ == "__main__":
